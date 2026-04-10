1c3d64071b2a32aa5ab1507d47a86d6bacc573c2
Nishanie Jayamaha <nishaniejayamaha@Administrators-MacBook-Air.local>
2026-04-11 01:17:22 +0200

    Ajout page Entreprises B2B, fix samosa, hero CTA corrigé

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   // Pass through static assets
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("entreprises");
   eleventyConfig.addPassthroughCopy({ "src/favicon.ico": "favicon.ico" });
   eleventyConfig.addPassthroughCopy("src/delices-pos-sw.js");
 
